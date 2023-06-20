610ec9706d65194f0a228341f086fb2c08351cc2
Aditya Dahal <toadityadahal@gmail.com>
2023-06-20 14:03:02 +0545

    Service For Blogs

--- src/main/java/com/blogapp/blogapp/repository/BlogRepo.java
+++ src/main/java/com/blogapp/blogapp/repository/BlogRepo.java
@@ -1,2 +1,8 @@
-package com.blogapp.blogapp.repository;public class BlogRepo {
+package com.blogapp.blogapp.repository;
+
+import com.blogapp.blogapp.DTO.BlogDto;
+import com.blogapp.model.Blog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogRepo extends JpaRepository<BlogDto, Integer> {
 }
--- src/main/java/com/blogapp/blogapp/service/BlogService.java
+++ src/main/java/com/blogapp/blogapp/service/BlogService.java
@@ -1,2 +1,17 @@
-package com.blogapp.blogapp.service;public class service {
+package com.blogapp.blogapp.service;
+import com.blogapp.blogapp.DTO.BlogDto;
+import com.blogapp.blogapp.repository.BlogRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+public class BlogService {
+    @Autowired
+    BlogRepo blogRepo;
+
+    public ResponseEntity<BlogDto> saveBlogs(@RequestBody BlogDto blogDto) {
+        return ResponseEntity.ok(blogRepo.save(blogDto));
+    }
 }
